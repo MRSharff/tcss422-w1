4831c08da2db950aa79608c791e34b6049f87f31
BrunoCode <luiss3@uw.edu>
2016-04-01 14:06:16 -0700

    test constructor and init

--- pcb_test.c
+++ pcb_test.c
@@ -1,9 +1,24 @@
 #include "pcb.h"
 /** Driver for testing all methods for pcb.
-*/
+  */
 int main(int theArgC, char* theArgV[]){
+    //test pcb constructor
+    PCB_p test1 = NULL;
+    test1 = PCB_construct();
+    if(test1 == NULL){
+        puts("Constructor failed");
+    } else {
+        puts("Constructor passed");
+    }
 
-
-}
-
-
+    //test initializer
+    PCB_init(test1);
+    if(tes1->pid == DEFAULT_PID
+        && test->state == NEW
+        && test->priority == DEFAULT_PRIORITY
+        && test->pc == DEFAULT_PC
+      ) {
+        puts("intitializer passed test");
+    }
+        
+ } 
