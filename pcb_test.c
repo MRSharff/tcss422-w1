a4a9c06e2cf21e8be646662527c3209ad7a9021f
BrunoCode <luiss3@uw.edu>
2016-04-01 22:25:49 -0700

    make constructor test a function

--- pcb_test.c
+++ pcb_test.c
@@ -1,31 +1,54 @@
 #include "pcb.h"
-/** Driver for testing all methods for pcb.
-  */
-int main(int theArgC, char* theArgV[]){
-    //test pcb constructor
-    PCB_p test1 = NULL;
-    test1 = PCB_construct();
-    if(test1 == NULL){
-        puts("Constructor failed");
+/**
+	Checks if construction is successful.
+ */
+void testConstructor(){
+	PCB_p t1 = PCB_construct();
+    if(t1){
+		puts("Constructor passed");
+		free(t1);
     } else {
-        puts("Constructor passed");
+        puts("Constructor failed");
     }
+}
 
+/** Driver for testing all methods for pcb.
+ */
+int main(int theArgC, char* theArgV[]){
+    char str[100];
+	//test pcb constructor
+	testConstructor();
     //test initializer
-    PCB_init(test1);
+    PCB_init(t1);
     if(tes1->pid == DEFAULT_PID
         && test->state == NEW
         && test->priority == DEFAULT_PRIORITY
         && test->pc == DEFAULT_PC
       ) {
-        puts("intitializer passed test");
+        puts("initializer passed test");
     }
     
     //test set pid
-    PCB_set_pid(test1, 123456789UL);
-    if(test1->pid == 123456789UL){
-        puts("Setting pid was suceesful");
+    PCB_set_pid(t1, 123456789UL);
+    if(t1->pid == 123456789UL){
+        puts("Setting pid was successful");
     } else {
         puts("setting pid failed");
     }
+	
+	//test get pid
+	unsigned long temp_pid = PCB_get_pid(t1);
+	if(temp_pid == 123456789UL){
+		puts("get pid worked");
+	} else {
+		puts("get pid failed");
+	}
+	
+	//test pcb_toString
+	PCB_toString(t1, str);
+	if (strcmp(str, "pid: 123456789, state: 0, priority: 31, pc: 0")) {
+		puts("toString passed");
+	} else {
+		puts("toString failed");
+	}
  } 
