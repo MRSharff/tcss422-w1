77e367406196be81bf0b5a6c24f666d9eb969be6
BrunoCode <luiss3@uw.edu>
2016-04-03 21:58:40 -0700

    move testing logic into individual functions
    Create a function for each test. This is modular design.
    Still needs work. I need the pcb.h file to be updated to
    include the pcb struct. I also need to know the final values
    the initializer sets to test the toString properly.

--- pcb_test.c
+++ pcb_test.c
@@ -1,7 +1,9 @@
 #include "pcb.h"
-/**
-	Checks if construction is successful.
- */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/** Checks if construction is successful.*/
 void testConstructor(){
 	PCB_p t1 = PCB_construct();
     if(t1){
@@ -11,44 +13,62 @@ void testConstructor(){
         puts("Constructor failed");
     }
 }
-
-/** Driver for testing all methods for pcb.
- */
-int main(int theArgC, char* theArgV[]){
-    char str[100];
-	//test pcb constructor
-	testConstructor();
-    //test initializer
-    PCB_init(t1);
+/** Checks if initialized correctly. */
+void testInitializer() {
+	PCB_p t1 = PCB_construct();
+	PCB_init(t1);
     if(tes1->pid == DEFAULT_PID
         && test->state == NEW
         && test->priority == DEFAULT_PRIORITY
         && test->pc == DEFAULT_PC
       ) {
         puts("initializer passed test");
-    }
-    
-    //test set pid
-    PCB_set_pid(t1, 123456789UL);
+    } else {
+		puts("initializer failed")
+	}
+}
+/** Checks if setting pid works. */
+void testSetPid() {
+	PCB_p t1 = PCB_construct();
+	PCB_init(t1);
+	PCB_set_pid(t1, 123456789UL);
     if(t1->pid == 123456789UL){
         puts("Setting pid was successful");
     } else {
         puts("setting pid failed");
     }
-	
-	//test get pid
+}
+/** Test if the get_pid retrieves correct pid. */
+void testGetPid() {
+	PCB_p t1 = PCB_construct();
+	PCB_init(t1);
+	PCB_set_pid(t1, 123456789UL);
 	unsigned long temp_pid = PCB_get_pid(t1);
 	if(temp_pid == 123456789UL){
 		puts("get pid worked");
 	} else {
 		puts("get pid failed");
 	}
-	
-	//test pcb_toString
-	PCB_toString(t1, str);
-	if (strcmp(str, "pid: 123456789, state: 0, priority: 31, pc: 0")) {
+}
+/** Checks if toString correctly represents
+	the pcb state*/
+void testToString(char * theStr){
+	PCB_p t1 = PCB_construct();
+	PCB_init(t1);
+	PCB_set_pid(t1, 123456789UL);
+	PCB_toString(t1, theStr);
+	if (strcmp(theStr, "pid: 123456789, state: 0, priority: 31, pc: 0")) {
 		puts("toString passed");
 	} else {
 		puts("toString failed");
-	}
- } 
+	}	
+}
+/** Driver for testing all methods of pcb.*/
+int main(int theArgC, char* theArgV[]){
+    char str[100];
+	testConstructor();
+    testInitializer();
+	testSetPid();
+	testGetPid();
+	testToString(str);
+} 
