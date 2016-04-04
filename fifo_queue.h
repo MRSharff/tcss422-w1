15311c66ccc3442b3bae065a51a116d49ff07066
Vitaliy Radchishin <vitaliyrd@gmail.com>
2016-04-04 12:22:12 -0700

    Partial implementation

---
+++ fifo_queue.h
@@ -0,0 +1,17 @@
+#ifndef FIFO_QUEUE
+#define FIFO_QUEUE
+
+typedef struct FIFO_q {
+    int size;
+    struct node* front;
+    struct node* rear;
+} FIFOq;
+typedef FIFOq* FIFOq_p;
+
+FIFOq_p FIFOq_construct(void);
+int FIFOq_init(FIFOq_p queue);
+int FIFOq_is_empty(FIFOq_p queue);
+void FIFOq_enqueue(FIFOq_p queue);
+int FIFOq_dequeue(FIFOq_p queue);
+
+#endif
