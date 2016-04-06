e097dcea9129b53e90b3992e319b05d1057b6dab
BrunoCode <luiss3@uw.edu>
2016-04-05 21:22:27 -0700

    import files

---
+++ ready_queue_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "ready_queue.h"
+#include "fifo_queue.h"
+
+int main(int theArgC, char * theArgV[]) {
+	char str[200];
+	
+	return 0;
+}
